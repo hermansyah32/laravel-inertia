7cfd6935e8bccf15520f9e14fdf92e783190e0ea
Hermansyah <herman.piliang23@gmail.com>
2022-10-05 17:04:55 +0700

    console bug fix

--- routes/console.php
+++ routes/console.php
@@ -34,13 +34,21 @@ Artisan::command('app:clear', function () {
 
 Artisan::command('app:install {--R|reset}', function ($reset) {
     if ($reset) {
-        $this->comment("Application reset");
+        Artisan::call('migrate:fresh');
+        $this->info("Database migrated");
+        Artisan::call('db:seed');
+        $this->info("Database seed");
+        Artisan::call('storage:link');
+        $this->info("Symbolic link finished");
+        Artisan::call('manifest:generate');
+        $this->info('Logo & manifest file generated');
+        $this->info("Application installed");
     } else {
         Artisan::call('migrate');
         $this->info("Database migrated");
         Artisan::call('db:seed');
         $this->info("Database seed");
-        Artisan::call('storage;link');
+        Artisan::call('storage:link');
         $this->info("Symbolic link finished");
         Artisan::call('manifest:generate');
         $this->info('Logo & manifest file generated');
