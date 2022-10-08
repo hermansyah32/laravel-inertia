2c7002fb7cf0e7a78e6ad5ba5d59e200ff624bf7
Hermansyah <herman.piliang23@gmail.com>
2022-10-08 18:52:57 +0700

    override exception json message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use App\Http\Response\BodyResponse;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,7 +47,47 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (env('APP_ENV') === 'prod') {
+                if (app()->bound('sentry')) {
+                    app('sentry')->captureException($e);
+                }
+            }
         });
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        $body = new BodyResponse();
+        $body->setResponseAuthFailed();
+        return $this->shouldReturnJson($request, $exception)
+            ? response()->json($body->getResponse(), $body->getResponseCode()->value)
+            : redirect()->guest($exception->redirectTo() ?? route('login'));
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException  $e, $request)
+    {
+        if ($e->response) {
+            return $e->response;
+        }
+
+        $body = new BodyResponse();
+        $body->setResponseValidationError($e->errors(), $e->getMessage());
+        return $this->shouldReturnJson($request, $e)
+            ? response()->json($body->getResponse(), $body->getResponseCode()->value)
+            : $this->invalid($request, $e);
     }
 }
