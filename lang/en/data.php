fa5418075faa22bf2756ab18b1a339afedad8c08
Hermansyah <herman.piliang23@gmail.com>
2022-10-13 16:28:32 +0700

    edit language resources

--- lang/en/data.php
+++ lang/en/data.php
@@ -17,8 +17,12 @@ return [
     'created' => ':Data successfully created.',
     'deleted' => ':Data successfully moved to trashed.',
     'force_deleted' => ':Data successfully deleted.',
-    'recovered' => ':Data successfully recovered.',
+    'restored' => ':Data successfully recovered.',
     'reset' => ':Data password successfully reset.',
     'validation' => ':Data validation doesn\'t match..',
+    'unauthorized' => 'You are not authorized. Please login or contact support',
 
+    // Section default data name
+    'profile' => 'Profile',
+    'account' => 'Account',
 ];
---
+++ lang/en/modifier.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helper
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to modify data result string.
+    |
+    */
+
+    'account_status' => [
+        'active' => 'Active',
+        'non active' => 'Non Active',
+        'blocked' => 'Blocked'
+    ],
+
+];
