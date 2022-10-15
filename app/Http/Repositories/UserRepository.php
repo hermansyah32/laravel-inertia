4bc5ca550decb4a597d248f16ab07dd563119be7
Hermansyah <herman.piliang23@gmail.com>
2022-10-15 15:33:31 +0700

    feature: user repository

---
+++ app/Http/Repositories/UserRepository.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Http\Repositories\Base\BaseRepository;
+use App\Http\Response\BodyResponse;
+use App\Models\User;
+use Illuminate\Container\Container as Application;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Str;
+
+class UserRepository extends BaseRepository
+{
+    /**
+     * Base repository Constructor
+     *
+     * @param Application $app
+     * @param string $messageResponseKey
+     * @throws Exception
+     */
+    public function __construct(Application $app)
+    {
+        parent::__construct($app, Lang::get('data.user'));
+    }
+
+    /**
+     * Set model class
+     * @return Model
+     */
+    public function model()
+    {
+        return $this->model = User::class;
+    }
+
+    /**
+     * Get create validation
+     * @return object Object of rules, messages, attributes. 
+     */
+    public function createValidation()
+    {
+        return ((object) [
+            'rules' => [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'email', 'max:255', 'unique:users'],
+                'username' => ['nullable', 'string', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8'],
+            ],
+            'messages' => [],
+            'attributes' => []
+        ]);
+    }
+
+    /**
+     * Get update validation
+     * @return object Object of rules, messages, attributes. 
+     */
+    public function updateValidation()
+    {
+        return ((object) [
+            'rules' => [
+                'name' => ['required', 'string', 'max:255']
+            ],
+            'messages' => [],
+            'attributes' => []
+        ]);
+    }
+
+    /**
+     * Reset current user password
+     *
+     * @param string $column Where column
+     * @param string|int|float $value Keyword value
+     * @param bool $author Update data with author status
+     * @return BodyResponse
+     */
+    public function reset(string $column, string|int|float $value,  bool $author = false): BodyResponse
+    {
+        $body = new BodyResponse();
+        try {
+            $randomPassword = Str::random(8);
+            $model = $this->findBy($column, $value);
+            if (!$model) $body->setResponseNotFound($this->messageResponseKey);
+            if ($author) $this->insertAuthor(true, $model);
+
+            $model->password = Hash::make($randomPassword);
+            $model->save();
+
+            $body->setBodyMessage($this->messageResponse['successUpdated']);
+        } catch (\Throwable $th) {
+            $body->setResponseError($th->getMessage());
+        }
+        return $body;
+    }
+}
---
+++ tests/Feature/Repositories/UserRepositoryTest.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Tests\Feature\Repositories;
+
+use App\Http\Repositories\UserRepository;
+use App\Http\Response\BodyResponse;
+use App\Http\Response\ResponseCode;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_users_index_repository()
+    {
+        $user = User::factory()->create();
+
+        /** @var BodyResponse $result */
+        $result =  App::call(function (UserRepository $repository) {
+            return $repository->index();
+        });
+
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertCount(1, $result->getBodyData());
+    }
+
+    public function test_users_index_trashed_repository()
+    {
+        $user = User::factory()->create();
+        $user->delete();
+
+        /** @var BodyResponse $result */
+        $result =  App::call(function (UserRepository $repository) {
+            return $repository->indexTrashed();
+        });
+
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertCount(1, $result->getBodyData());
+    }
+
+    public function test_users_get_repository()
+    {
+        $user = User::factory()->create();
+
+        /** @var BodyResponse $result */
+        $result =  App::call(function (UserRepository $repository) use ($user) {
+            return $repository->get('id', $user->id);
+        });
+
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertSame($user->name, $result->getBodyData()->name);
+    }
+
+    public function test_users_get_trashed_repository()
+    {
+        $user = User::factory()->create();
+        $user->delete();
+
+        /** @var BodyResponse $result */
+        $result =  App::call(function (UserRepository $repository) use ($user) {
+            return $repository->getTrashed('id', $user->id);
+        });
+
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertSame($user->name, $result->getBodyData()->name);
+    }
+
+    public function test_users_update_repository()
+    {
+        $user = User::factory()->create();
+        $updatedUser = [
+            'name' => 'Ameera Raimkar'
+        ];
+
+        /** @var BodyResponse $result */
+        $result =  App::call(function (UserRepository $repository) use ($user, $updatedUser) {
+            return $repository->updateBy($updatedUser, 'id', $user->id);
+        });
+
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertSame($updatedUser['name'], $result->getBodyData()->name);
+    }
+
+    public function test_users_restore_repository()
+    {
+        $user = User::factory()->create();
+        $user->delete();
+
+        /** @var BodyResponse $result */
+        $result = App::call(function (UserRepository $repository) use ($user) {
+            return $repository->restoreBy('id', $user->id);
+        });
+
+        $userFind = User::where('id', $user->id)->first();
+
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertSame($user->name, $userFind->name);
+    }
+
+    public function test_users_delete_repository()
+    {
+        $user = User::factory()->create();
+
+        /** @var BodyResponse $delete */
+        $delete = App::call(function (UserRepository $repository) use ($user) {
+            return $repository->deleteBy('id', $user->id);
+        });
+
+        $userFind = User::where('id', $user->id)->first();
+        $userDeleted = User::onlyTrashed()->where('id', $user->id)->first();
+
+        $this->assertInstanceOf(BodyResponse::class, $delete);
+        $this->assertSame($delete->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertNull($userFind);
+        $this->assertSame($userDeleted->name, $user->name);
+    }
+
+    public function test_users_delete_permanently_repository()
+    {
+        $user = User::factory()->create();
+
+        /** @var BodyResponse $delete */
+        $delete = App::call(function (UserRepository $repository) use ($user) {
+            return $repository->permanentDeleteBy('id', $user->id);
+        });
+
+        $userFind = User::where('id', $user->id)->first();
+        $userDeleted = User::onlyTrashed()->where('id', $user->id)->first();
+
+        $this->assertInstanceOf(BodyResponse::class, $delete);
+        $this->assertSame($delete->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertNull($userFind);
+        $this->assertNull($userDeleted);
+    }
+
+    public function test_users_reset_repository()
+    {
+        $this->markTestIncomplete('Reset password already done in database, but new random password should be send to user via email. Email notification for new password is not complete yet');
+
+        $user = User::factory()->create();
+        $isPassword = Hash::check('password', $user->password);
+
+        /** @var BodyResponse $delete */
+        $result = App::call(function (UserRepository $repository) use ($user) {
+            return $repository->reset('id', $user->id);
+        });
+
+        $userFind = User::where('id', $user->id)->first();
+        $checkNewPassword = Hash::check('password', $userFind->password);
+
+        $this->assertTrue($isPassword);
+        $this->assertInstanceOf(BodyResponse::class, $result);
+        $this->assertSame($result->getResponseCode()->value, ResponseCode::OK->value);
+        $this->assertFalse($checkNewPassword);
+    }
+}
