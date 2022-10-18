5d2c906e79e15d68e6b2807093c27afd8c84cd4f
Hermansyah <herman.piliang23@gmail.com>
2022-10-18 16:09:32 +0700

    remove: ziggy routes

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,9 +39,9 @@ class HandleInertiaRequests extends Middleware
                 'user' => $request->user(),
             ],
             'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
+                return [
                     'location' => $request->url(),
-                ]);
+                ];
             },
             'flash' => [
                 'title' => session()->get('flash_title'),
