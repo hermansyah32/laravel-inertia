319ab9dced85a3933394b6855e3db93b77c2ccff
Hermansyah <herman.piliang23@gmail.com>
2022-11-06 14:50:00 +0700

    add: subject content

---
+++ database/factories/SubjectContentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SubjectContent>
+ */
+class SubjectContentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => 'Content ' . fake()->randomLetter(),
+            'content' => fake()->paragraph(),
+            'subject_group_id' => fake()->randomNumber(),
+        ];
+    }
+}
