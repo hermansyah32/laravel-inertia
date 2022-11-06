53b82d2ebe540ed63f8e62ce1591d5366bca73c8
Hermansyah <herman.piliang23@gmail.com>
2022-11-06 14:54:40 +0700

    add: authentication log notification view

---
+++ resources/views/vendor/authentication-log/emails/new.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+# @lang('Hello!')
+
+@lang('Your :app account logged in from a new device.', ['app' => config('app.name')])
+@component('mail::table')
+{{ '|        |          |' }}
+{{ '| ------ | -------- |' }}
+{{ '| ' . trans('Account') . ' | ' . $account->email . ' |' }}
+@if ($location && $location['default'] === false)
+{{ '| ' . trans('City') . ' | ' . $location['city'] ?? __('Unknown City') . ' |'}}
+{{ '| ' . trans('State') . ' | ' . $location['state'], __('Unknown State') . ' |'}}
+@endif
+{{ '| ' . trans('Time Login') . ' | ' . $time->toCookieString() . ' |'}}
+{{ '| ' . trans('Browser') . ' | ' . $browser . ' |'}}
+@endcomponent
+
+@lang('If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.')<br/>
+
+@lang('Regards')<br/>
+{{ config('app.name') }}
+@endcomponent
